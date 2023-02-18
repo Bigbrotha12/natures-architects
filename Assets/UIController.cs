34df5f29701a468ec180feb5ce39b429f8910c05
richie-oates <richoates2020@gmail.com>
2023-02-18 17:49:59 +0100

    Show next character images

--- Assets/UIController.cs
+++ Assets/UIController.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
     [SerializeField] GameObject gameOverPanel;
     [SerializeField] GameObject successPanel;
 
+    [Header("Next Character Images")]
+    [SerializeField] Image[] nextImages;
 
     void Awake()
     {
@@ -27,6 +30,12 @@ public class UIController : MonoBehaviour
         ShowSuccessPanel(true);
     }
 
+    void HideGameOverPanels()
+    {
+        gameOverPanel.SetActive(false);
+        successPanel.SetActive(false);
+    }
+
     void ShowGameOverPanel(bool show)
     {
         gameOverPanel.SetActive(show);
@@ -40,15 +49,35 @@ public class UIController : MonoBehaviour
     public void QuitToStart()
     {
         EventBroker.CallReturnToTitleScreen();
+        HideGameOverPanels();
     }
 
     public void RestartLevel()
     {
         EventBroker.CallRestartLevel();
+        HideGameOverPanels();
     }
 
     public void NextLevel()
     {
         EventBroker.CallLoadNextLevel();
+        ShowSuccessPanel(false);
+    }
+
+    public void SetNextCharacterSprites(int currentCharacterID, GameLevelSO currentLevel)
+    {
+        int index = currentCharacterID + 1;
+
+        for (int i = 0; i < nextImages.Length; i++, index++)
+        {
+            if (index < currentLevel.AvailableCharacters.Length)
+            {
+                nextImages[i].sprite = currentLevel.AvailableCharacters[index].CharacterSO.defaultSprite;
+            }
+            else
+            {
+                nextImages[i].sprite = null; 
+            }
+        }
     }
 }

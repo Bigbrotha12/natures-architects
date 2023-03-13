2a3c40978a698df3dd3204b97bc21c7b4645039c
richie-oates <richoates2020@gmail.com>
2023-03-13 08:13:46 +0100

    Create IPlayerProgressData.cs

---
+++ Assets/Scripts/Game/IPlayerProgressData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public interface IPlayerProgressData
+{
+    public ILevelProgressData GetLevelProgress(int levelID);
+}
+
+public interface ILevelProgressData
+{
+    public int StarsAwarded();
+    public int HighScore();
+    public bool Completed();
+    public bool Available();
+    public Sprite MapImage();
+}
